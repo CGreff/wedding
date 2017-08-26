35d24e191ff73dad643812c1aab858a421965d02
Chris <npreston@pillartechnology.com>
2017-08-26 14:29:10 -0400

    Got a form setup, posting content that doesnt work yet

--- src/webapp/js/app.js
+++ src/webapp/js/app.js
@@ -1,10 +1,24 @@
 const serve = require('koa-static')
+const koaBody = require('koa-body')
+const Router = require('koa-router')
 const Koa = require('koa')
 const app = new Koa()
+const router = new Router()
 
-app.use(serve('src/webapp/static'))
-app.use(serve('bower_components'))
+router.post('/rsvp', function (ctx, next) {
+    console.log(ctx);
+
+    ctx.status = 200
+  })
+
+//Serve Static Components including default route for index.html
+app
+    .use(serve('src/webapp/static'))
+    .use(serve('bower_components'))
+    .use(router.routes())
+    .use(router.allowedMethods());
+
 var port =  process.env.PORT || 3000;
 app.listen(port);
 
-console.log('listening on port 3000')
+console.log('listening on port ' + port);
